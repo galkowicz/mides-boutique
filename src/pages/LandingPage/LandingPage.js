a5e297ad8a006f0939e67b238d51b1ee6e6b0968
Orian <orian.galk@gmail.com>
2021-08-10 01:29:08 +0300

    feature: add logo and footer

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,8 +2,11 @@ import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+import InstagramIcon from '@material-ui/icons/Instagram'
 import { makeStyles } from '@material-ui/core/styles'
 import meatBackground from '../../assets/meat-background.jpeg'
+import midesLogo from '../../assets/midesLogo.png'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,6 +43,32 @@ const useStyles = makeStyles((theme) => ({
     width: '15rem',
     margin: 'auto',
   },
+  content: {
+    backgroundColor: '#fff',
+  },
+  footer: { // TODO make footer fixed to button and no background on mobile
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'space-between',
+    padding: '0 2rem',
+    backgroundColor: '#fff',
+  },
+  logoContainer: {
+    width: '5rem',
+  },
+  logo: {
+    maxWidth: '100%',
+    height: 'auto',
+  },
+  link: {
+    color: "#e96b1e"
+  },
+  phoneNumber: {
+    fontFamily: ['Heebo', 'sans-serif'].join(','),
+    padding: '.5rem',
+    fontSize: '1rem',
+  },
 }))
 
 export default function LandingPage() {
@@ -55,27 +84,44 @@ export default function LandingPage() {
     >
       <CssBaseline />
       <Grid
-        className={classes.text}
-        item
+        container
+        alignItems="center"
+        justifyContent="center"
+        className={classes.content}
       >
-        <Typography className={classes.title} variant="h2" align="center">
-          מידס בוטיק
-        </Typography>
-        <Typography className={classes.subtitle1} variant="h4" align="center">
-          חוויה קולינרית ברזילאית לכם ולאוהבים שלכם
-        </Typography>
-        <Typography className={classes.subtitle2} variant="h5" align="center">
-          הפתיחה בקרוב
-        </Typography>
+        <Grid className={classes.text} item>
+          <Typography className={classes.title} variant="h2" align="center">
+            מידס בוטיק
+          </Typography>
+          <Typography className={classes.subtitle1} variant="h4" align="center">
+            חוויה קולינרית ברזילאית לכם ולאוהבים שלכם
+          </Typography>
+          <Typography className={classes.subtitle2} variant="h5" align="center">
+            הפתיחה בקרוב
+          </Typography>
+        </Grid>
+        <Grid className={classes.image} item>
+          <picture>
+            <source srcSet={meatBackground} />
+            <img alt="meat background" />
+          </picture>
+        </Grid>
       </Grid>
-      <Grid
-        className={classes.image}
-        item
-      >
-        <picture>
-          <source srcSet={meatBackground} />
-          <img src="/media/cc0-images/painted-hand-298-332.jpg" alt="meat background" />
+      <Grid className={classes.footer}>
+        <picture className={classes.logoContainer}>
+          <source srcSet={midesLogo} />
+          <img className={classes.logo} alt="mides logo" />
         </picture>
+        <Typography className={classes.phoneNumber} variant="h4" align="center">
+          0546744197
+        </Typography>
+        <Link
+          className={classes.link}
+          href="https://www.instagram.com/mides_brazilian_restaurant"
+          target="_blank"
+        >
+          <InstagramIcon className={classes.instagram} aria-label="delete" />
+        </Link>
       </Grid>
     </Grid>
   )
